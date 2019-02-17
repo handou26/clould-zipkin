1e53c506b233f62c1e0a88d8085bd2ced6299e91
handou26 <251585741@qq.com>
2019-02-17 17:15:49 +0800

    链路追踪

---
+++ src/main/java/com/jd/ClouldZipkinApplication.java
@@ -0,0 +1,19 @@
+package com.jd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+import zipkin2.server.internal.EnableZipkinServer;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableZipkinServer
+public class ClouldZipkinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClouldZipkinApplication.class, args);
+	}
+
+}
+
